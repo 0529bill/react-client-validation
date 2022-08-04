31498c508b449dfcbbe47a82f2b0332b24ebbb25
bill <0529bilL@gmail.com>
2022-08-04 15:23:38 +0800

    feat: fix ts

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+export interface ErrorObject {
+  index: string
+  condition: (string | number | boolean | undefined | null)[]
+  errorMessage?: string
+  customCondition?: (dataSource: unknown, conditionReturnArray: boolean[]) => boolean[]
+  errorFormat?: string[]
+}
+export declare type ErrorResult = {
+  msg: string
+}
+export declare type ErrorValidation = {
+  [key: string | number]: ErrorResult | '' | string | null | (string | ErrorResult | null)[]
+}
+export declare const handleValidation: ({
+  errorArray,
+  dataSource,
+  defaultErrorMessage,
+}: {
+  errorArray: ErrorObject[]
+  dataSource?: unknown
+  defaultErrorMessage?: string | undefined
+}) => (boolean | ErrorValidation)[]
